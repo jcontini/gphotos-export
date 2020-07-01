6edfd8fff38d3dd9163c870d92545cde61eee460
Joe <efisio@gmail.com>
2020-07-01 03:39:15 -0700

    improve parsing

--- gphotos_export/utils.py
+++ gphotos_export/utils.py
@@ -6,16 +6,17 @@ db = sqlite_utils.Database(db_file)
 def index_zip_media(zf):
     with zipfile.ZipFile(zf, 'r') as archive:
         zipname = os.path.basename(zf)
-        print("Reading: "+zipname)
+        print(">> Indexing: "+zipname)
         media=[];meta=[]
         files = archive.namelist()
         for filepath in files:
             ext = os.path.splitext(filepath)[1]
             if ext == '.json':
-                meta.append({
-                    'file':filepath,
-                    'archive':zipname
-                })
+                if 'metadata' not in filepath:
+                    meta.append({
+                        'file':filepath,
+                        'archive':zipname
+                    })
             else:
                 media.append({
                     'file':filepath,
@@ -23,33 +24,40 @@ def index_zip_media(zf):
                     'archive':zipname,
                     'hangouts':1 if 'Hangout_' in filepath else 0
                     })
-        db['index'].insert_all(media,alter=True)
-        db['meta'].insert_all(meta,alter=True,pk="file")
+        db['index'].upsert_all(media,alter=True,pk="file")
+        db['meta'].upsert_all(meta,alter=True,pk="file")
 
 def pull_metadata(path):
-    db.create_view("archives", """select archive from meta group by archive""")
+    if "archives" not in db.view_names():
+        db.create_view("archives", """select archive from meta group by archive""")
     for row in db["archives"].rows:
+        print(">> Parsing metadata from %s..." % row["archive"])
         with zipfile.ZipFile(path+row["archive"], 'r') as archive:
             for row in db["meta"].rows_where("archive = ?", [row["archive"]]):
                 with archive.open(row["file"]) as metafile:
-                    print(row["file"])
                     r = json.loads(metafile.read())
                     try:
                         d = {
                             "title": r["title"],
-                            "description": r["description"],
-                            "ts_taken": r["photoTakenTime"]["formatted"],
-                            "ts_created": r["creationTime"]["formatted"],
-                            "ts_modified": r["modificationTime"]["formatted"],
+                            "ts_taken": int(r["photoTakenTime"]["timestamp"]),
+                            "ts_created": int(r["creationTime"]["timestamp"]),
+                            "ts_modified": int(r["modificationTime"]["timestamp"]),
+                            "tsf_taken": r["photoTakenTime"]["formatted"],
+                            "tsf_created": r["creationTime"]["formatted"],
+                            "tsf_modified": r["modificationTime"]["formatted"],
                             "geo_lat": r["geoData"]["latitude"],
+                            "geoExif_lat": r["geoDataExif"]["latitude"],
                             "geo_long": r["geoData"]["longitude"],
+                            "geoExif_long": r["geoDataExif"]["longitude"],
                             "geo_alt": r["geoData"]["altitude"],
-                            "trashed": r.get("trashed")
+                            "geoExif_alt": r["geoDataExif"]["altitude"],
+                            "description": r["description"],
+                            "imageViews": int(r.get("imageViews",0)),
+                            "trashed": r.get("trashed",0)
                         }
-                        print(json.dumps(d,indent=2))
                         db["meta"].update(row["file"],d, alter=True)
                     except:
-                        print("Skipping...")
+                        print("Issue parsing %s" % row["file"])
                         pass
 
 def enumerate_zips(path):
