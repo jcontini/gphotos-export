b75b6c514fb9d377263d48830591562ad998bc5e
Joe <efisio@gmail.com>
2020-07-05 16:04:45 -0700

    Significantly better metafile matching

--- gphotos_export/utils.py
+++ gphotos_export/utils.py
@@ -1,4 +1,4 @@
-import os, glob, json, sqlite_utils, zipfile
+import os, glob, json, sqlite_utils, zipfile, re
 from sqlite_utils.db import NotFoundError
 
 db_file = 'data.db'
@@ -7,7 +7,6 @@ db = sqlite_utils.Database(db_file)
 def index_zip_media(zf):
     with zipfile.ZipFile(zf, 'r') as archive:
         zipname = os.path.basename(zf)
-        print(">> Indexing: "+zipname)
         media=[];meta=[]
         files = archive.namelist()
         for filepath in files:
@@ -23,7 +22,8 @@ def index_zip_media(zf):
                     'file':filepath,
                     'ext': str.upper(ext[1:]),
                     'archive':zipname,
-                    'hangouts':1 if 'Hangout_' in filepath else 0
+                    'hangouts':1 if 'Hangout_' in filepath else 0,
+                    'edited':0
                     })
         db['media'].upsert_all(media,alter=True,pk="file")
         db['meta'].upsert_all(meta,alter=True,pk="file")
@@ -58,26 +58,64 @@ def pull_metadata(path):
                         print("Issue parsing %s" % row["file"])
                         pass
 
+def check_meta(metapath):
+    try:
+        db["meta"].get(metapath)
+        return True
+    except NotFoundError:
+        return False
+
 def match_meta():
-    num_match=0;num_nomatch=0
+    if "nomatch" not in db.view_names():
+        db.create_view("nomatch", """select * from media where metapath is null and edited is not 1""")
+
     for r in db['media'].rows:
-        filepath, ext = os.path.splitext(r['file'])
-        filepath = filepath.split('(')[0]
-        if filepath[-7:] == '-edited':
-            metapath = filepath[:-7] + ext + '.json'
-            #TODO: Fix case problem
-        else:
-            metapath = filepath + ext + '.json'
-        try:
-            metadata = db["meta"].get(metapath)
-            d = {"metapath": metapath}
-            db['media'].update(r['file'],d, alter=True)
-            num_match +=1
-        except NotFoundError:
-            print('%s != %s' % (r['file'],metapath))
-            num_nomatch +=1
-    print("%s medias matched with metadata" % num_match)
-    print("%s medias with no match" % num_nomatch)
+        fullpath, ext = os.path.splitext(r['file'])
+        folder = fullpath.rsplit('/',1)[0] + '/'
+        filename = fullpath.rsplit('/',1)[1] + ext
+
+        #Skip edited versions
+        if '-edited' in filename:
+            db['media'].update(r['file'],{"edited":1},alter=True)
+            continue
+        
+        #Try the default json path
+        metapath = folder + filename +'.json'
+        if check_meta(metapath):
+            db['media'].update(r['file'],{"metapath": metapath}, alter=True)
+            continue
+
+        #Handle case: Metafile trims filename at 46 characters
+        if len(filename) > 46:
+            filename = filename[:46]
+            metapath = folder + filename +'.json'
+            if check_meta(metapath):
+                db['media'].update(r['file'],{"metapath": metapath}, alter=True)
+                continue
+            
+        #Handle case: Hangouts with account_id drops extension
+        if 'account_id' in filename:
+            filename = filename.rsplit(ext,1)[0]
+            metapath = folder + filename +'.json'
+            if check_meta(metapath):
+                db['media'].update(r['file'],{"metapath": metapath}, alter=True)
+                continue
+
+        #Handle case: Different files ending in (1),(2) etc drop extension if non-() exists
+        re_pattern = '\(\d\)\.'
+        re_matches = re.findall(re_pattern,filename)
+        if len(re_matches) > 0:
+            meta1 = fullpath.rsplit('(',1)[0] + ext + '.json'
+            if check_meta(meta1):
+                metapath = meta1
+                db['media'].update(r['file'],{"metapath": metapath}, alter=True)
+                continue
+
+        print("No match: %s" % filename)
+
+    unmatched = db['nomatch'].count
+    print('--- Media Report ---')
+    print("%s remaining with no match" % unmatched)
         
 def fullrun(path):
     zipfiles = glob.glob(path+'*.zip')
