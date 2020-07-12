23c0b6b7ca4f8754b16d2e726e870501620a7570
Joe <efisio@gmail.com>
2020-07-11 22:27:15 -0700

    Pseudocode to export files

--- gphotos_export/utils.py
+++ gphotos_export/utils.py
@@ -16,38 +16,38 @@ def index_zip_media(zf):
             if ext == '.json':
                 if not any(x in filepath for x in nomedia):
                     meta.append({
-                        'path':filepath,
+                        'meta_path':filepath,
                         'archive':zipname
                     })
             elif ext != '.html':
                 media.append({
-                    'path':filepath,
+                    'media_path':filepath,
                     'filename': filepath.rsplit('/',1)[1],
                     'ext': str.upper(ext[1:]),
                     'archive':zipname,
                     'edited':0
                     })
-        db['media_files'].upsert_all(media,alter=True,pk="path")
-        db['meta_files'].upsert_all(meta,alter=True,pk="path")
+        db['media_files'].upsert_all(media,alter=True,pk="media_path")
+        db['meta_files'].upsert_all(meta,alter=True,pk="meta_path")
 
 def get_media_meta(path):
     print(">> Parsing metadata from archives...")
-    if "archives" not in db.view_names():
-        db.create_view("archives", """select archive from meta_files group by archive""")
-    for row in db["archives"].rows:
+    if "archives_meta" not in db.view_names():
+        db.create_view("archives_meta", """select archive from meta_files group by archive""")
+    for row in db["archives_meta"].rows:
         with zipfile.ZipFile(path+row["archive"], 'r') as archive:
             for row in db['meta_files'].rows_where("archive = ?", [row["archive"]]):
-                with archive.open(row["path"]) as metafile:
+                with archive.open(row["meta_path"]) as metafile:
                     r = json.loads(metafile.read())
                     try:
-                        if 'metadata' in row["path"]:
+                        if 'metadata' in row["meta_path"]:
                             # Extract Album info
                             d = {
                                 "type": 'album',
                                 "title": r["albumData"]["title"],
                                 "description": r["albumData"]["description"]
                             }
-                            db['meta_files'].update(row["path"],d, alter=True)
+                            db['meta_files'].update(row["meta_path"],d, alter=True)
                         else:
                             # Extract Media info
                             ts_taken = time.gmtime(int(r["photoTakenTime"]["timestamp"]))
@@ -66,10 +66,10 @@ def get_media_meta(path):
                                 "imageViews": int(r.get("imageViews",0)),
                                 "trashed": r.get("trashed",0)
                             }
-                            db['meta_files'].update(row["path"],d, alter=True)
+                            db['meta_files'].update(row["meta_path"],d, alter=True)
 
                     except:
-                        print("Issue parsing %s" % row["path"])
+                        print("Issue parsing %s" % row["meta_path"])
                         pass
 
 def check_meta(metapath):
@@ -85,23 +85,23 @@ def match_meta():
         db.create_view("nomatch", """select * from media_files where metapath is null and edited is not 1""")
     if "matches" not in db.view_names():
         db.create_view("matches", """SELECT * FROM media_files as media
-                                    LEFT JOIN meta_files AS meta ON media.metapath=meta.path
+                                    LEFT JOIN meta_files AS meta ON media.metapath=meta.meta_path
                                     WHERE metapath is not null""")
 
     for r in db['media_files'].rows:
-        fullpath, ext = os.path.splitext(r['path'])
+        fullpath, ext = os.path.splitext(r['media_path'])
         folder = fullpath.rsplit('/',1)[0] + '/'
         filename = fullpath.rsplit('/',1)[1] + ext
 
         #Skip edited versions
         if '-edited' in filename:
-            db['media_files'].update(r['path'],{"edited":1},alter=True)
+            db['media_files'].update(r['media_path'],{"edited":1},alter=True)
             continue
         
         #Try the default json path
         metapath = folder + filename +'.json'
         if check_meta(metapath):
-            db['media_files'].update(r['path'],{"metapath": metapath}, alter=True)
+            db['media_files'].update(r['media_path'],{"metapath": metapath}, alter=True)
             continue
 
         #Handle case: Metafile trims filename at 46 characters
@@ -109,7 +109,7 @@ def match_meta():
             filename = filename[:46]
             metapath = folder + filename +'.json'
             if check_meta(metapath):
-                db['media_files'].update(r['path'],{"metapath": metapath}, alter=True)
+                db['media_files'].update(r['media_path'],{"metapath": metapath}, alter=True)
                 continue
             
         #Handle case: Hangouts with account_id drops extension
@@ -117,7 +117,7 @@ def match_meta():
             filename = filename.rsplit(ext,1)[0]
             metapath = folder + filename +'.json'
             if check_meta(metapath):
-                db['media_files'].update(r['path'],{"metapath": metapath}, alter=True)
+                db['media_files'].update(r['media_path'],{"metapath": metapath}, alter=True)
                 continue
 
         #Handle case: When google adds (1), (2) etc to filename
@@ -126,13 +126,13 @@ def match_meta():
         if len(re_matches) > 0:
             metapath = fullpath.rsplit('(',1)[0] + ext + '.json'
             if check_meta(metapath):
-                db['media_files'].update(r['path'],{"metapath": metapath}, alter=True)
+                db['media_files'].update(r['media_path'],{"metapath": metapath}, alter=True)
                 continue
 
         #Handle other cases where the extension is dropped in metapath
         metapath = fullpath + '.json'
         if check_meta(metapath):
-            db['media_files'].update(r['path'],{"metapath": metapath}, alter=True)
+            db['media_files'].update(r['media_path'],{"metapath": metapath}, alter=True)
             continue
 
         #Catch-all
@@ -148,8 +148,9 @@ def prep_folder_structure():
     print('>> Preparing new folder structure...')
     re_pattern = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
     for r in db['media_files'].rows_where("metapath is not null"):
-        current_folder = r['path'].rsplit('/',2)[1]
+        current_folder = r['media_path'].rsplit('/',2)[1]
         meta = db['meta_files'].get(r['metapath'])
+        lib_add = 0
 
         # If Hangouts, put in /Hangouts folder
         if current_folder[:8] == 'Hangout_':
@@ -163,37 +164,61 @@ def prep_folder_structure():
         # If current folder has 0000-00-00 format, put in Library/[year] folders
         elif re.match(re_pattern,current_folder[:10]):
             folder = 'Library/' + str(meta['year'])
+            lib_add = 1
 
         # Otherwise, put in Album folder
         else:
             folder = 'Albums/' + current_folder
-        db['media_files'].update(r['path'],{"newfolder": folder}, alter=True)
+        db['media_files'].update(r['media_path'],{"newfolder": folder, "lib_add": lib_add}, alter=True)
 
 def add_album_media():
     print('>> Scanning Albums for images not in library...')
     library = []
     for r in db['matches'].rows_where("newfolder LIKE 'Library/%'"):
-        library.append({'filename': r['filename'],'ts_taken': r['ts_taken']})
+        library.append({'filename': r['filename'],'ts_taken': r['ts_taken'], 'media_path': r['media_path']})
 
     for r in db['matches'].rows_where("newfolder LIKE 'Albums/%'"):
-        lib_add = 1
+        lib_add = 1;source = ''
         lib_filenames = [i for i in library if i['filename'] == r['filename']]
         if len(lib_filenames) > 0:
-            print(lib_filenames)
             for possible_match in lib_filenames:
                 if r['ts_taken'] == possible_match['ts_taken']:
                     lib_add = 0
+                    source = possible_match['media_path']
 
-        db['media_files'].update(r['path'],{"lib_add": lib_add}, alter=True)
+        db['media_files'].update(r['media_path'],{"lib_add": lib_add, "source": source}, alter=True)
+
+def prep_folder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
 def write_exif():
     todo = '''
-    - for each row in export:
-        - create folder if not exists
-        - open the media file
-        - update the exif data
-        - save the file
+    - open the media file
+    - update the exif data
+    - save the file
     '''
+
+def export_files(path):
+    print(">> Extracting files with EXIF data to new folder structure...")
+    if "archives_media" not in db.view_names():
+        db.create_view("archives_media", """select archive from media_files group by archive""")
+    for row in db["archives_media"].rows:
+        with zipfile.ZipFile(path+row["archive"], 'r') as archive:
+            for row in db['matches'].rows:
+                with archive.open(row["meta_path"]) as metafile:
+                    pass #todo
+
+    for r in db['matches'].rows:
+        prep_folder(r['newfolder'])
+        #TODO zip.extract pathfile to newfolderpath
+        #write_exif('...')
+        if r['newfolder'][7:] == 'Albums/' and r['lib_add'] == 1:
+            lib_folder = 'Library/' + str(r['year'])
+            prep_folder(lib_folder)
+            #TODO zip.extract pathfile to newfolderpath
+            #write_exif('...')
+
 
 def fullrun(path):
     zipfiles = glob.glob(path+'*.zip')
@@ -203,6 +228,7 @@ def fullrun(path):
     match_meta()
     prep_folder_structure()
     add_album_media()
+    #export_files(path)
 
 # Notes to make clear to users:
 # /Trashed contains deleted files. NOT in lib
